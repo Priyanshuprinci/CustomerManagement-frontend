0531602a81463b4b3873491855e862c622a09878
Priyanshuprinci <princi.it.js@gmail.com>
2025-03-16 14:38:09 +0530

    Updated backend API

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:9000',
+  baseURL: process.env.REACT_APP_API_URL || 'https://customermanagement-backend.onrender.com/api',
 });
 
 export const fetchCustomers = (params) => API.get('/customers', { params });
